090ed46412240c8859e51dfdab352f36207c5f74
moemaair <rageali12@gmail.com>
2023-09-25 20:47:47 +0300

    model class pic

---
+++ src/main/java/controller/Pic.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class Pic {
+    public String id;
+    public String file;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+}
--- src/main/java/controller/SavepicController.java
+++ src/main/java/controller/SavepicController.java
@@ -1,8 +1,13 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SavepicController {
-
+    @GetMapping("/")
+    public String hello() {
+        return "hey faya";
+    }
 }
