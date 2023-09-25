45c5c38c5a6b334e2aa913a7076e4f6b3402002b
moemaair <rageali12@gmail.com>
2023-09-25 20:36:39 +0300

    initial commit

---
+++ src/main/java/com/personal/mohamed/savepics/SavepicsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.mohamed.savepics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavepicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavepicsApplication.class, args);
+	}
+
+}
